79e89eff6aa12c362985da21140777f7c1309ae6
anthonydohan <132645190+anthonydohan@users.noreply.github.com>
2023-06-05 22:22:00 +0700

    Web Film Lokal

---
+++ app.js
@@ -0,0 +1,13 @@
+<body>
+  <!-- Konten halaman series -->
+
+  <div class="back-home">
+    <button id="back-home-btn">Kembali ke Home</button>
+  </div>
+
+  <script>
+    document.getElementById('back-home-btn').addEventListener('click', function() {
+      window.location.href = 'index.html'; // Ganti 'index.html' dengan halaman utama Anda
+    });
+  </script>
+</body>
